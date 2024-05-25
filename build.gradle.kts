1081f12f8e3107d00211728d5fb7a00f92fed3de
Slava <plumsoftwareofficial@gmail.com>
2024-05-25 16:20:53 +0600

    [^] Повысил версию плагина сериализации

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val macExtraPlistKeys: String
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.24"
     id("app.cash.sqldelight") version "2.0.2"
 }
 
