c1a6a0c160c049ebc7a2d6c3f28cfc534b43cc43
Slava <plumsoftwareofficial@gmail.com>
2025-02-13 13:39:57 +0600

    Сделал многомодульную архитектуру

---
+++ ui/core/theme-ui-module/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(project(path = ":client-core:local-store-module"))
+
+    implementation(compose.desktop.currentOs)
+
+    implementation(globalVersions.material3)
+
+//    JUnit
+    testImplementation(globalVersions.junit)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
